12ef9720f6b2175a9dce797ba0bef5988065708b
Omer <omerfarukdasar@gmail.com>
2022-04-14 18:49:24 +0200

    yarn lock deleted

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,14 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContext";
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 const Main = () => {
+  const currentUser = useContext(AuthContext);
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const getMovies = (API) => {
     axios
       .get(API)
@@ -15,9 +18,28 @@ const Main = () => {
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim() && currentUser) {
+      getMovies(SEARCH_API + searchTerm);
+    } else if (!currentUser) {
+      alert("Please login to search a movie");
+    } else {
+      alert("please enter a text");
+    }
+  };
   return (
     <>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
       <div className="flex justify-content-center flex-wrap">
         {movies.map((movie) => (
           <MovieCard key={movie.id} {...movie} movie={movie} />
