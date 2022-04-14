50a5618ceecd1740c0968d94b23372855385479b
Omer <omerfarukdasar@gmail.com>
2022-04-14 16:17:23 +0200

    details page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
+  const { id } = useParams();
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
+  const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+  const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+  useEffect(() => {
+    axios
+      .get(movieDetailBaseUrl)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, [movieDetailBaseUrl]);
   return <div>MovieDetail</div>;
 };
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
 import Register from "../pages/Register";
 
 const AppRouter = () => {
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </Router>
   );
