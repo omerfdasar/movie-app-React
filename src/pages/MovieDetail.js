67e49c8feadaa6c1a67794ef6c563c8461a5d541
Omer <omerfarukdasar@gmail.com>
2022-04-14 16:33:19 +0200

    details

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 const MovieDetail = () => {
   const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState();
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
@@ -14,10 +15,51 @@ const MovieDetail = () => {
   useEffect(() => {
     axios
       .get(movieDetailBaseUrl)
-      .then((res) => console.log(res.data))
+      .then((res) => setMovieDetails(res.data))
       .catch((err) => console.log(err));
   }, [movieDetailBaseUrl]);
-  return <div>MovieDetail</div>;
+  return (
+    <div className="container py-5">
+      <h1 className="text-center">{movieDetails?.title}</h1>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={
+                movieDetails?.poster_path
+                  ? baseImageUrl + movieDetails?.poster_path
+                  : defaultImage
+              }
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title">Overview</h5>
+              <p className="card-text">{movieDetails?.overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                {"Release Date : " + movieDetails?.release_date}
+              </li>
+              <li className="list-group-item">
+                {"Rate : " + movieDetails?.vote_average}
+              </li>
+              <li className="list-group-item">
+                {"Total Vote : " + movieDetails?.vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1} className="card-link">
+                  Go Back
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetail;
